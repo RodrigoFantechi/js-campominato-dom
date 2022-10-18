cb08580957c55848e2108ade78ba9a67fdf8b60c
RodrigoFantechi <rodrifante595@libero.it>
2022-10-18 13:18:36 +0200

    comment console.log

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,8 +43,8 @@ function generaGriglia(whereGemerateGrid, howManycells, bombs, whereGenerateResu
         
         // check bombs
         square.addEventListener('click', function () {
-            console.log(bombs);
-            console.log(square.innerHTML);
+            // console.log(bombs);
+            // console.log(square.innerHTML);
             if(bombs.includes(Number(square.innerHTML))){
                 for (let i = 0; i < bombs.length; i++) {
                     const bomb = bombs[i];
